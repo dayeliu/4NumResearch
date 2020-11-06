610398ec19b40df73a22e8137bfb2e5188a45bd1
KYLE.KT.LIU <KYLE.KT.LIU@deltaww.com>
2020-11-06 15:33:35 +0800

    [update] 2020/11/06

--- src/main/java/stock/master/app/service/Impl/CrawlCmoney.java
+++ src/main/java/stock/master/app/service/Impl/CrawlCmoney.java
@@ -7,28 +7,21 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 import org.springframework.stereotype.Service;
 import stock.master.app.service.BaseService;
-
+import com.google.gson.Gson;
 
 @Service
 public class CrawlCmoney extends BaseService {
 
-	final private String appid = "20201027171244144";
-	final private String appsecret = "92938700183411ebba92000c29beef84";
-	
-	final private String token_url = "https://owl.cmoney.com.tw/OwlApi/auth";
-	final private String token_param = "appId=" + appid + "&appSecret=" + appsecret;
-	
 	/*
 	 * https://owl.cmoney.com.tw/Owl/tutorial/list.do?id=4f44a640131911e9befb000c29e493f4
 	 * https://dotblogs.com.tw/grayyin/2016/04/07/100152
 	 * 
 	 * */
-	public void testing () throws Exception {
-		
+	public String getToken () throws Exception {
+		String token_url = "https://owl.cmoney.com.tw/OwlApi/auth";
 		URL obj = new URL(token_url);
 		HttpURLConnection urlConnection = (HttpURLConnection) obj.openConnection();
 		urlConnection.setDoOutput(true);
@@ -36,7 +29,10 @@ public class CrawlCmoney extends BaseService {
 		urlConnection.setRequestMethod("POST");
 		urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-		OutputStream os = urlConnection.getOutputStream();
+		String appid = "20201027171244144";		// from cmoney
+		String appsecret = "92938700183411ebba92000c29beef84";	// from comney
+		String token_param = "appId=" + appid + "&appSecret=" + appsecret;
+		OutputStream os = urlConnection.getOutputStream(); // send data
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
 		writer.write(token_param);
 		writer.flush();
@@ -46,7 +42,6 @@ public class CrawlCmoney extends BaseService {
 		urlConnection.connect();
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
-
 		StringBuffer sb = new StringBuffer("");
 		String lines="";
 		while ((lines = reader.readLine()) != null) {
@@ -57,7 +52,42 @@ public class CrawlCmoney extends BaseService {
 		reader.close();
 		urlConnection.disconnect();
 		
+		String tok = sb.toString();
+		Gson gson = new Gson();
+		auth object = gson.fromJson(tok, auth.class);
 		
+		return object.token;
 	}
 
+	/*
+	 * 
+	 * */
+	private void getData(String tok) throws Exception {
+		String data_url = "https://owl.cmoney.com.tw/OwlApi/api/v2/json/PYPRI-14847a/2330/40";
+		URL obj = new URL(data_url);
+		HttpURLConnection urlConnection = (HttpURLConnection) obj.openConnection();
+		urlConnection.setDoOutput(true);
+		urlConnection.setDoInput(true);
+		urlConnection.setRequestMethod("GET");
+		urlConnection.setRequestProperty("authorization", "Bearer " + tok); // header
+		urlConnection.setRequestProperty("Content-Type", "application/json");
+		urlConnection.connect();
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
+
+		StringBuffer sb = new StringBuffer("");
+		String lines="";
+		while ((lines = reader.readLine()) != null) {
+			lines = new String(lines.getBytes(), "utf-8");
+			//sb.append( lines);
+			System.out.println(lines);
+		}
+		//System.out.println(sb);
+		reader.close();
+		urlConnection.disconnect();
+	}
+}
+
+class auth {
+	String token;
 }
